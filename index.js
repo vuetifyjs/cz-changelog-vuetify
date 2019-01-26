a5f28965be8c66a2046da669fd69cc047e21ecf1
sh7dm <d3dx12.xx@gmail.com>
2019-01-26 09:02:05 +0300

    feat: add kitchen type

--- index.js
+++ index.js
@@ -3,8 +3,14 @@
 var engine = require('./engine')
 var conventionalCommitTypes = require('conventional-commit-types')
 
+var types = conventionalCommitTypes.types
+types.kitchen = {
+  description: 'Related to kitchen',
+  title: 'Kitchen'
+}
+
 module.exports = engine({
-  types: conventionalCommitTypes.types,
+  types: types,
   defaultType: process.env.CZ_TYPE,
   defaultScope: process.env.CZ_SCOPE,
   defaultSubject: process.env.CZ_SUBJECT,
